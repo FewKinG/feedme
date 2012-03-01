91fb99a376b49857a2e251fa315af6a6520e0165
Sven C <fewking@paniccrew.de>
2012-03-02 00:16:53 +0100

    save even if element value hash is nil

--- app/controllers/feedme_controller.rb
+++ app/controllers/feedme_controller.rb
@@ -3,7 +3,14 @@ class FeedmeController < ApplicationController
   def create
     model = params[:model]
     klass = model.classify.constantize
-    obj = klass.new(params[:elements])
+    obj = klass.new
+    if params[:elements]
+      params[:elements].each do |k,v|
+	if k
+	  obj.send("#{k.to_s}=", v)
+	end
+      end
+    end
     @feedme_object = obj
     @success = obj.save 
     render :partial => "feedme/create_#{model.to_s}"
