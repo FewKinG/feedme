a0e2b5c172456fbef601e5b3afd69a9bb4324d0c
Sven C <fewking@paniccrew.de>
2012-03-02 00:04:36 +0100

    make feedback element accessible

--- app/controllers/feedme_controller.rb
+++ app/controllers/feedme_controller.rb
@@ -4,6 +4,7 @@ class FeedmeController < ApplicationController
     model = params[:model]
     klass = model.classify.constantize
     obj = klass.new(params[:elements])
+    @feedme_object = obj
     @success = obj.save 
     render :partial => "feedme/create_#{model.to_s}"
   end
